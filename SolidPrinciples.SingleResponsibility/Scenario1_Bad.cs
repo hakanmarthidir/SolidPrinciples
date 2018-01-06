2f9ca0c4be5ec30be5b56a915290947b0a637e74
hakan mart hidir <hakan_hidir@hotmail.com>
2018-01-06 17:13:34 +0300

    Add project files.

---
+++ SolidPrinciples.SingleResponsibility/Scenario1_Bad.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.SingleResponsibility
+{
+    //Notice:
+    //One reason to change. if there is more than one reason to change, Refactor it.
+
+    //SCENARIO 1 -> Bad 
+    public class Dealer
+    {
+        public string DealerName { get; set; }
+        public string DealerPassword { get; set; }
+
+        void Login()
+        {
+            //Operation 1  : codes for checking user on db 
+            using (SqlConnection con = new SqlConnection(""))
+            {
+                //and other codes here.
+            }
+            //Operation 2  : logging codes something about operation 1
+            using (StreamWriter w = File.AppendText("log.txt"))
+            {
+                w.WriteLine("");
+            }
+        }
+    }
+
+}
---
+++ SolidPrinciples.SingleResponsibility/Scenario2_Good.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.SingleResponsibility
+{
+    //SCENARIO 2 -> Good -> we are seperating the operations
+    
+    //A.
+    public class Distributor
+    {
+        public string DistributorName { get; set; }
+        public string DistributorPassword { get; set; }      
+    }
+
+    //B.
+    public class DistributorManager
+    {
+        public void Login(Distributor client)
+        {
+            using (SqlConnection con = new SqlConnection(""))
+            {
+                //login codes here
+            }
+        }
+    }
+
+    //C. 
+    public class LogManager
+    {
+        public void LogOperation(string loggingText)
+        {
+            //write to anywhere
+        }
+    }
+}
