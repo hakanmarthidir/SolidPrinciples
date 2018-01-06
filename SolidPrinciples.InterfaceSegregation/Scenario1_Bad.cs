26ee3e0cc8e44797b28528c08c737c9478afd859
hakan mart hidir <hakan_hidir@hotmail.com>
2018-01-06 21:48:10 +0300

    Interface Segregation

---
+++ SolidPrinciples.InterfaceSegregation/Scenario1_Bad.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.InterfaceSegregation
+{   
+   
+    public interface IClient
+    {
+        void CreateOrder();
+        void GetReports();
+        void SaleToCustomer();
+    }
+    //Important : Dealer may not have permissions for getting reports but IClient inteface has GetReports function!! 
+    public class Dealer : IClient
+    {
+        public void CreateOrder()
+        {
+            Console.WriteLine("Order could be inserted.");
+        }
+        public void GetReports()
+        {
+            throw new NotImplementedException();
+        }
+        public void SaleToCustomer()
+        {
+            Console.WriteLine("Retail Sale informations could be inserted.");
+        }
+    }
+
+    public class Distributor : IClient
+    {
+        public void CreateOrder()
+        {
+            Console.WriteLine("Order could be inserted.");
+        }
+        public void GetReports()
+        {
+            Console.WriteLine("Report is preparing.");
+        }
+        public void SaleToCustomer()
+        {
+            Console.WriteLine("Retail Sale informations could be inserted.");
+        }
+    }
+
+    //** We need to change our approaching. Look at Scenario 2 =>
+
+}
---
+++ SolidPrinciples.InterfaceSegregation/Scenario2_Good.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace SolidPrinciples.InterfaceSegregation
+{
+    //Single Responsibility and Interface Segregation
+
+    public interface ISeller
+    {
+        void SaleToCustomer();
+    }
+    public interface ICreator
+    {
+        void CreateOrder();        
+    }
+    public interface IReporter
+    {
+        void GetReports();
+    }
+
+    public class Vendor : ISeller, ICreator
+    {
+        public void CreateOrder()
+        {
+            Console.WriteLine("Order could be inserted.");
+        }
+        public void SaleToCustomer()
+        {
+            Console.WriteLine("Retail Sale informations could be inserted.");
+        }
+    }
+    public class HeadOffice : ISeller, IReporter, ICreator
+    {
+        public void CreateOrder()
+        {
+            Console.WriteLine("Order could be inserted.");
+        }
+        public void GetReports()
+        {
+            Console.WriteLine("Report is preparing.");
+        }
+        public void SaleToCustomer()
+        {
+            Console.WriteLine("Retail Sale informations could be inserted.");
+        }
+    }
+}
