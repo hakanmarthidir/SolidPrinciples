db7b434ef5c38b6589be4ad52e43019f7b6c0561
hakan mart hidir <hakan_hidir@hotmail.com>
2018-01-06 17:49:12 +0300

    open closed principle

---
+++ SolidPrinciples.OpenClosed/Scenario1_Bad.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.OpenClosed
+{
+    //Notice :  Codes must be open to extension, close to modification. 
+
+    //BAD SCENARIO
+    public class XCar
+    {
+        public void Start() { }
+    }
+
+    public class XHelicopter
+    {
+        public void Start() { }
+    }
+
+    public class EngineManager
+    {
+        public void StartEngine(XCar car)
+        {
+            car.Start();
+        }
+
+        public void StartEngine(XHelicopter helicopter)
+        {
+            helicopter.Start();
+        }
+
+        // or we can write this
+        public void StartEngine(object engine)
+        {
+            if (engine is XCar)
+            {
+
+            }
+            else if (engine is XHelicopter)
+            {
+
+            }
+            //Important Point :  if we add a new type like Plane, EngineManager can not start Plane's engine. 
+            //And now we have to add a new else if block for Plane class. 
+        }
+
+
+    }
+
+
+
+}
---
+++ SolidPrinciples.OpenClosed/Scenario2_Good.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciples.OpenClosed
+{
+    //GOOD SCENARIO
+    public abstract class Vehicle
+    {
+        public abstract void Start();
+    }
+    public class Car : Vehicle
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Helicopter : Vehicle
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+    public class Truck : Vehicle
+    {
+        public override void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class VehicleEngineManager
+    {
+        public void Start(Vehicle vehicle)
+        {
+            vehicle.Start();
+        }
+    }
+
+    //And now we can add a new vehicle type. "Open to extension"
+    //We will not change the VehicleEngineManager codes. because of, VehicleEngineManager's Start method uses Vehicle type parameter.  "Closed to modification"
+
+
+}
